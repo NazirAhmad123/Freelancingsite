45ed54c5483d7ac8bb02c917146eb1a5e23b9430
NBK-14-011 <NBK-14-011@NBK-14-011>
2018-10-08 11:33:01 +0800

    First commit

---
+++ mail/contact_us.php
@@ -0,0 +1,13 @@
+<?php 
+$ToEmail = 'jawidfirotan@gmail.com'; 
+$EmailSubject = 'Site contact form'; 
+$mailheader = "From: ".$_POST["email"]."\r\n"; 
+$mailheader .= "Reply-To: ".$_POST["email"]."\r\n"; 
+$mailheader .= "Content-type: text/html; charset=iso-8859-1\r\n"; 
+$MESSAGE_BODY = "Name: ".$_POST["name"].""; 
+$MESSAGE_BODY .= "Email: ".$_POST["email"].""; 
+$MESSAGE_BODY .= "Message: ".nl2br($_POST["message"]).""; 
+mail($ToEmail, $EmailSubject, $MESSAGE_BODY, $mailheader) or die ("Failure"); 
+
+echo "<h3>Thanks for contacting us we will get in touch with you very soon!</h3>";
+?>
